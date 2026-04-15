056e932174ce9a3bf3195f8e1fbe194bfc37f81a
Bandit8238 <zech.sq@gmail.com>
2026-04-14 18:34:38 -0600

    app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
+import { loadGames } from "../systems/games.js";
+import { initDashboard } from "../ui/dashboard.js";
 
+async function init() {
+  const games = await loadGames();
+  initDashboard(games);
+}
+
+init();
