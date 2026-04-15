470c45015a2d564e75379eb518bf67915ac77055
Bandit8238 <zech.sq@gmail.com>
2026-04-14 18:36:47 -0600

    dashboard.js

--- ui/dashboard.js
+++ ui/dashboard.js
@@ -1,1 +1,10 @@
+import { createTile } from "../../components/Tile.js";
 
+export function initDashboard(games) {
+  const app = document.getElementById("app");
+
+  games.forEach(game => {
+    const tile = createTile(game);
+    app.appendChild(tile);
+  });
+}
