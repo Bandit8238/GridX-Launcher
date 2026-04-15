840e1aa27a59b4221b74fa5282ded246729acf10
Bandit8238 <zech.sq@gmail.com>
2026-04-14 18:37:22 -0600

    gridNavigation.js

--- ui/gridNavigation.js
+++ ui/gridNavigation.js
@@ -1,1 +1,10 @@
+let index = 0;
 
+export function initGrid(tiles) {
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowRight") index++;
+    if (e.key === "ArrowLeft") index--;
+
+    tiles[index]?.classList.add("focused");
+  });
+}
